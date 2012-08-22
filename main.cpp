4ddaff4eda194cc709a211f85360ab0b099fef73
Erfan Loghmani <elkiscoming@users.sf.net>
2012-08-22 21:29:06 +0430

    remove everything from main and added Manager::run

--- main.cpp
+++ main.cpp
@@ -1,15 +1,8 @@
-#include "WorldModel.h"
-#include "Gui.h"
+#include "Manager.h"
 #include <iostream>
 using namespace std;
 int main(){
-	cerr << "Arman is directing ...\n";
-	WorldModel m(3 , 0.5);
-	GUI ui;
-	while(true) {
-		ui.clear();
-		ui.draw(m);
-		ui.update();
-	}
+	Manager m;
+	m.run();
 	return 0;
 }
