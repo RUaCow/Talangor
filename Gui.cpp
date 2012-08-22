402cf81572100de56bd037e1aca0085d9cd90b67
Erfan Loghmani <elkiscoming@users.sf.net>
2012-08-22 19:56:50 +0430

    sink with float ball radius

--- Gui.cpp
+++ Gui.cpp
@@ -1,6 +1,7 @@
 #include "Gui.h"
 #include "WorldModel.h"
-
+#include <iostream>
+using namespace std;
 GUI::GUI() {
 	SDL_Init(SDL_INIT_VIDEO);
 	display = SDL_SetVideoMode(800, 600, 32, SDL_HWSURFACE);
@@ -17,8 +18,9 @@ void GUI::clear() {
 void GUI::draw(const WorldModel &wm) {
 	// Draw balls
 	for(int i = 0; i < wm.balls.size(); i ++)
-		circleRGBA(display, convert(wm.balls[i].pos).x(), convert(wm.balls[i].pos).y(), wm.balls[i].radius,
+		circleRGBA(display, convert(wm.balls[i].pos).x(), convert(wm.balls[i].pos).y(), wm.balls[i].radius * (float)display->w,
 				255, 255, 255, 255);
+	cerr << wm.balls[0].radius * (float) display->w << endl;
 }
 
 void GUI::update() {
