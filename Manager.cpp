93fac554d259bd8ce2c81db752c7d9fd0a10f900
Erfan Loghmani <elkiscoming@users.sf.net>
2012-08-23 10:51:33 +0430

    Added mouseRealese and call wm.update and ...

--- Manager.cpp
+++ Manager.cpp
@@ -9,6 +9,16 @@ Manager::~Manager(){
 
 void Manager::run(){
 	while(1){
+		pair<Vector2Df , Vector2Df> mouseR = ui.mouseRelease();
+		//test
+		/*static bool ali = true;
+		if(ali)
+			mouseR.first = wm.balls[0].pos;
+		ali = false;*/
+		int wb = wm.insideWichBall(mouseR.first);
+		if(wb != -1)
+			wm.addMove(wb , mouseR.first - mouseR.second);
+		wm.update();
 		ui.clear();
 		ui.draw(wm);
 		ui.update();
