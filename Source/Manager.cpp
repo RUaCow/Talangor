5d02accc5019e5dd5db95f568661bcf7d4d6a5fb
Erfan Loghmani <elkiscoming@users.sf.net>
2012-08-26 10:56:58 +0430

    handle mouse events... ready to play

--- Source/Manager.cpp
+++ Source/Manager.cpp
@@ -10,24 +10,19 @@ Manager::~Manager(){
 
 void Manager::run(){
 	while(1){
-		const Vector2Df zero(0, 0);
-		pair<Vector2Df , Vector2Df> mouseR = /*ui.mouseRelease()*/ make_pair(zero, zero);
-
-		//test
-		/*static bool ali = true;
-		if(ali)
-			mouseR.first = wm.balls[0].pos;
-		ali = false;*/
-
-		int wb = wm.insideWichBall(mouseR.first);
-		if(wb != -1)
-			wm.addMove(wb , mouseR.first - mouseR.second);
 		wm.update();
 
 		ui.clear();
 		ui.draw(wm);
 
 		GuiEvent event = ui.update();
+		if(event.click){
+			Vector2Df clickS = event.clickStart;
+			Vector2Df clickE = event.clickEnd;
+			int wb = wm.insideWichBall(clickS);
+			if(wb != -1)
+				wm.addMove(wb , clickS - clickE);
+		}
 		if(event.quitEvent)
 			break;
 	}
