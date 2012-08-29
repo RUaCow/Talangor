c328dfef3e722cd9e3e7d51d6700ef665b7acf2a
ArmaN <a.m.aegit@gmail.com>
2012-08-29 21:49:57 +0430

    'drawForceVector' is optimized.Uncomment line 44 if you want.

--- Source/ForceMeter.cpp
+++ Source/ForceMeter.cpp
@@ -16,37 +16,29 @@ void ForceMeter::drawForceVector(Vector2Df firstPoint , Vector2Df secondPoint)
 	//Length of the input.
 	float L = mainVector.getLength();
 
-	//Unit vector of the force vector.
-	Vector2Df unitFVector = mainVector.getNormalizedVector();
+	//alpha : the angle of the input.
+	float alpha = atan2f(mainVector.y() , mainVector.x());
 
-	//Unit vector of the prependicular vector to the unitFVector.
-	Vector2Df unitPVector = unitFVector.getPrependicularVector();
+	//theta : the angle of the arc of the force meter.
+	float theta = PI / 12;
 
-	for(float theta = 0.0f ; theta < PI / 12 ; theta += 0.01)
+	float Red = 0.0f , Green = 255.0f , Blue = 0.0f;
+	for(int l = 0 ; l <= L ; l++)
 	{
-		float Red = 0.0f , Green = 255.0f , Blue = 0.0f;
-		for(int l = 0 ; l <= L ; l++)
+		arcRGBA(screen , firstPoint.x() , firstPoint.y() , l , (alpha - theta) * 180 / PI , (alpha + theta) * 180 / PI , Red , Green , Blue , 255);
+		//Setting color for the next arc to draw.
+		if(l >= 0 && l < colorChangeLength)
 		{
-			//Vector of the specific point to draw.
-			Vector2Df pointVector = firstPoint + cosf(theta) * l * unitFVector + sinf(theta) * l * unitPVector;
-			pixelRGBA(screen , pointVector.x() , pointVector.y() , Red , Green , Blue , 255);
-			pointVector -= 2 * sinf(theta) * l * unitPVector;
-			pixelRGBA(screen , pointVector.x() , pointVector.y() , Red , Green , Blue , 255);
-
-			//Setting color for the next pixel to draw.
-			if(l >= 0 && l < colorChangeLength)
-			{
-				Green -= colorChangeRate;
-			}
-			else if(l >= colorChangeLength && l < 2 * colorChangeLength)
-			{
-				Green += colorChangeRate;
-				Red += colorChangeRate;
-			}
-			else
-			{
-				Green > 0 ? Green -= colorChangeRate : Green = 0;
-			}
+			Green -= colorChangeRate;
+		}
+		else if(l >= colorChangeLength && l < 2 * colorChangeLength)
+		{
+			Green += colorChangeRate;
+			Red += colorChangeRate;
+		}
+		else
+		{
+			Green > 0 ? Green -= colorChangeRate : Green = 0;
 		}
 		//Just for fun!!
 		//SDL_Flip(screen);
